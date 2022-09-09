7414881c055feef77c0c80e2429373a2ec12f405
lotuscyrus <johnsonsolotu@gmail.com>
2022-09-09 20:54:13 +0100

    commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./modules/commun/components/home/home.component";
+import { CoursesComponent } from "./modules/courses/components/Courses/courses.component";
+import { LoginComponent } from "./modules/authen/components/login/login.component";
+import { AboutComponent } from ".modules/commun/components/about/about.component";
+const routes: Routes =
+ [{
+    path:'',
 
-const routes: Routes = [];
+    component:HomeComponent
+
+},
+
+{
+  path:'Courses',
+  component:CoursesComponent
+},
+
+{
+  path:'Login',
+  component:LoginComponent
+},
+{
+  path:'About',
+  component:AboutComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
