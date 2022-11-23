ad296ba259359e462d053135c2401f174e11c18f
lotuscyrus <johnsonsolotu@gmail.com>
2022-11-23 17:15:08 +0100

    changes

--- src/app/modules/courses/components/courses/courses.component.ts
+++ src/app/modules/courses/components/courses/courses.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscriber, Subscription } from "rxjs";
+
+import { CategoryService } from "src/app/modules/commun/services/category.service";
+
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
+
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  categories:any[] = [];
 
-  ngOnInit(): void {
+  constructor(private serviceCategory:CategoryService) { }
+
+  ngOnInit(){
+    this.serviceCategory.getAllCategories()
+                         .subscribe(categories=>this.categories=categories );
   }
 
 }
