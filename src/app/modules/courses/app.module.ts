4a8478e60ccdb65eec78fc3fa9edac2053726847
lotuscyrus <johnsonsolotu@gmail.com>
2022-11-23 17:11:05 +0100

    changes

--- src/app/modules/courses/app.module.ts
+++ src/app/modules/courses/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule} from '@angular/core';
 import { CoursesComponent } from './components/courses/courses.component';
 import { MyMaterialModule } from 'src/app/material-ui.module';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+
 
 @NgModule({
  declarations: [
     CoursesComponent
   ],
-imports:[MyMaterialModule],
+imports:[BrowserModule, CommonModule,MyMaterialModule],
+exports:[CoursesComponent],
+
   providers: [],
   bootstrap: []
 })
